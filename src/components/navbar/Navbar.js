8a592c2b63cbe0400431b1ad399c66bf65ec4fd1
+ <+>
2022-07-28 21:18:39 +0300

    updated navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar navbar-expand-lg bg-info">
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+  </li>
+  <li class="nav-item">
+    <Link class="nav-link" to='/music'>Music</Link>
+  </li>
+  <li class="nav-item">
+    <Link class="nav-link" to='/search'>Search</Link>
+  </li>
+  </ul>  
+    </nav>
   )
 }
 
